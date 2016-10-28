484f776843a6dc9d52f129da97e5d73325df9776
~ponnys-podfer <iceman.ponies@gmail.com>
2016-10-27 22:23:48 -0700

    Add a script to install the files to a new urbit/copy back from an urbit.
    (I'm sinking moons fast enough to need this...)

---
+++ copy-from.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp $1/app/yint.hoon app/yint.hoon
+cp $1/gen/yint/*.hoon gen/yint/
+cp $1/lib/yint/*.hoon lib/yint/
+cp $1/sur/yint.hoon sur/
+
---
+++ copy-to.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp app/yint.hoon $1/app/yint.hoon
+cp -R gen/yint $1/gen/yint/
+cp -R lib/yint $1/lib/yint/
+cp sur/yint.hoon $1/sur/
